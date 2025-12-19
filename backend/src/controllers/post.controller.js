a573cc5340ba9c22292305dcd3c07d9909c03328
neeraj2117 <royal.nanu21@gmail.com>
2025-12-19 15:07:10 +0530

    small fixes

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,7 +1,10 @@
 import Post from "../models/post.model.js";
 import asyncHandler from "express-async-handler";
 import User from "../models/user.model.js";
+import { getAuth } from "@clerk/express";
 import cloudinary from "../config/cloudinary.js";
+import Notification from "../models/notification.model.js";
+import Comment from "../models/comment.model.js";
 
 export const getPosts = asyncHandler(async (req, res) => {
   try {
